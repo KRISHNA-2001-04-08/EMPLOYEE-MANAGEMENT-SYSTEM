d1d4459529d720972eba2238bc4adf7b38da801a
KRISHNA-2001-04-08 <krishnaramasamy284@gmailcom>
2025-12-04 19:51:10 +0530

    DATA ADDED

--- app/models.py
+++ app/models.py
@@ -3,9 +3,30 @@ from django.db import models
 class Employee(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    department = models.CharField(max_length=100)
-    salary = models.IntegerField()
-    join_date = models.DateField()
+
+    department = models.CharField(max_length=100, null=True, blank=True)
+    position = models.CharField(max_length=100, null=True, blank=True)
+    phone = models.CharField(max_length=15, null=True, blank=True)
+
+    city = models.TextField(null=True, blank=True)
+    state = models.CharField(max_length=100, null=True, blank=True)
+
+    gender = models.CharField(
+        max_length=10,
+        choices=[
+            ("Male", "Male"),
+            ("Female", "Female"),
+            ("Other", "Other"),
+        ],
+        null=True,
+        blank=True
+    )
+
+    salary = models.IntegerField(null=True, blank=True)
+    experience = models.IntegerField(default=0)
+
+    dob = models.DateField(null=True, blank=True)
+    join_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.name
